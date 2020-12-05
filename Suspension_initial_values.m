0087fbf9b42f9d6b9b9e99f09b5f9ad112f9b605
HP <HP@MANGLOOR>
2020-12-06 01:01:58 +0530

    Suspension system initial values

---
+++ Suspension_initial_values.m
@@ -0,0 +1,8 @@
+start_time=0;
+sampling_time=0.1;
+end_time=100;
+M1 = 1200;   % body mass (kg)
+k1 = 21000;  % suspension stiffness in (N/m)
+b = 2500;    %  suspension damping in (N sec/m)
+M2=13;       %wheel mass(kg)
+k2=320;      % suspension stiffness in (N/m)
